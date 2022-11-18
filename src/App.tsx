91145d13ecc02ab97b518a4c82745525771bf4b5
jolebowski <jordan.moreira@epitech.eu>
2022-11-18 11:52:04 +0100

    adding character and search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header.component";
 import Home from "./routes/home/home";
+import CharacterPage from "./routes/character/character";
 
 const App = () => {
   return (
-    <>
-      <Header />;
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/characters/:id" element={<Header />}>
+        <Route index element={<CharacterPage />} />
+      </Route>
+    </Routes>
   );
 };
 
